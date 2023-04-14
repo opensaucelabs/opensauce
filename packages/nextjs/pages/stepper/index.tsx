fc2f52189e92a16131b6952f7e842e44877a4ff4
hadzija7 <hadzijasale@gmail.com>
2023-04-14 20:31:01 +0200

    added step 2

--- packages/nextjs/pages/stepper/index.tsx
+++ packages/nextjs/pages/stepper/index.tsx
@@ -2,12 +2,43 @@ import * as React from "react";
 import { TextField } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Stepper from "@mui/material/Stepper";
 import Typography from "@mui/material/Typography";
 
-const steps = ["Connect your Github", "Create an ad group", "Create an ad"];
+const steps = ["Connect your Github", "Tokenize your repo", "Create an ad"];
+
+const BasicSelect = () => {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
 
 const step0 = () => {
   return (
@@ -39,9 +70,67 @@ const step0 = () => {
   );
 };
 
+const step1 = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "4rem",
+        marginBottom: "3rem",
+      }}
+    >
+      <div>
+        <div style={{ textAlign: "center", fontWeight: "800", fontSize: "20px" }}>Let&apos;s tokenize your repo</div>
+        <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+          Now we will configure the basics of the <br></br>
+          tokenization of your repository
+        </div>
+        <div>
+          <div style={{ width: "50%" }}>
+            <div style={{ fontWeight: "800", fontSize: "20px", marginBottom: "1rem" }}>
+              Select one of your repositories
+            </div>
+            <BasicSelect></BasicSelect>
+          </div>
+        </div>
+        <div>
+          <div style={{ fontWeight: "800", fontSize: "20px", marginTop: "3rem", marginBottom: "0px" }}>
+            Token configuration
+          </div>
+          <div style={{ justifyContent: "center", alignItems: "center", display: "flex" }}>
+            <div style={{ marginRight: "1rem", width: "50%" }}>
+              <div>Total supply</div>
+              <div>
+                <TextField style={{ width: "100%" }} id="outlined-basic" label="@github-username" variant="outlined" />
+              </div>
+              <div style={{ wordWrap: "break-word" }}>
+                Define the total supply of the token and the number of decimals
+              </div>
+            </div>
+            <div style={{ width: "50%", marginTop: "1.5rem" }}>
+              <div>Symbol(Token name)</div>
+              <div>
+                <TextField style={{ width: "100%" }} id="outlined-basic" label="@github-username" variant="outlined" />
+              </div>
+              <div style={{ wordWrap: "break-word" }}>
+                The symbol will have the prefix os (ie: osETH) and can be the maximum of 4 charachters long (ie: osXXXX)
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const renderSteps = (stepId: number) => {
   if (stepId == 0) {
     return step0();
+  } else if (stepId == 1) {
+    return step1();
   } else {
     return "";
   }
@@ -94,7 +183,7 @@ export default function HorizontalLinearStepper() {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: "100%", padding: "40px" }}>
       <Stepper activeStep={activeStep} style={{ marginTop: "1rem" }}>
         {steps.map((label, index) => {
           const stepProps: { completed?: boolean } = {};
