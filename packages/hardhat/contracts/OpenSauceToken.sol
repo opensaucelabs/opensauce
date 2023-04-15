dadb58efc2c15a7e3935e1b2c68975ae0887df51
Michael Bucher <michael@axlabs.com>
2023-04-15 14:59:16 +0200

    Fix read-access to contributor names and rewrite github to lowercase h consistently

--- packages/hardhat/contracts/OpenSauceToken.sol
+++ packages/hardhat/contracts/OpenSauceToken.sol
@@ -10,35 +10,38 @@ contract OpenSauceToken is Ownable, ERC20 {
     
     uint public lastDistribution;
     string[] public contributorNames;
+    mapping(string => bool) _isContributor;
 
     uint8 _decimals;
-    string _gitHubUrl;
+    string _githubUrl;
     address _creator;
     uint _creatorGitHubId;
 
     mapping(string => uint256) _claimable;
     mapping(string => uint256) _totalRewarded;
 
-    IGitHubLink _gitHubLinkContract;
+    IGitHubLink _githubLinkContract;
 
     constructor(
+        address owner,
         string memory name,
         string memory symbol,
         uint8 decimals_,
-        string memory gitHubUrl_,
-        address gitHubLinkContractAddress
+        string memory githubUrl_,
+        address githubLinkContractAddress
     )
         ERC20(name, symbol)
     {
+        _transferOwnership(owner);
         _decimals = decimals_;
-        _gitHubUrl = gitHubUrl_;
-        _setGitHubLinkContract(gitHubLinkContractAddress);
+        _githubUrl = githubUrl_;
+        _setGithubLinkContract(githubLinkContractAddress);
     }
 
     // public methods
 
-    function setGitHubLinkContract(address _address) public onlyOwner {
-        _setGitHubLinkContract(_address);
+    function setGithubLinkContract(address _address) public onlyOwner {
+        _setGithubLinkContract(_address);
     }
 
     function distribute(
@@ -49,8 +52,13 @@ contract OpenSauceToken is Ownable, ERC20 {
         require(arrLength == amounts.length, "invalid parameters");
         for (uint i = 0; i < arrLength; i++) {
             uint256 claimable_ = amounts[i];
-            _claimable[usernames[i]] += claimable_;
-            _totalRewarded[usernames[i]] += claimable_;
+            string memory username = usernames[i];
+            _claimable[username] += claimable_;
+            _totalRewarded[username] += claimable_;
+            if (!_isContributor[username]) {
+                contributorNames.push(username);
+                _isContributor[username] = true;
+            }
         }
         lastDistribution = block.number;
     }
@@ -71,8 +79,16 @@ contract OpenSauceToken is Ownable, ERC20 {
         return _totalRewarded[username];
     }
 
-    function gitHubUrl() public view returns (string memory) {
-        return _gitHubUrl;
+    function allContributorNames() public view returns (string[] memory) {
+        return contributorNames;
+    }
+
+    function nrContributers() public view returns (uint) {
+        return contributorNames.length;
+    }
+
+    function githubUrl() public view returns (string memory) {
+        return _githubUrl;
     }
 
     function decimals()
@@ -87,13 +103,12 @@ contract OpenSauceToken is Ownable, ERC20 {
 
     // private methods
 
-    function _setGitHubLinkContract(address _address) private {
-        _gitHubLinkContract = IGitHubLink(_address);
+    function _setGithubLinkContract(address _address) private {
+        _githubLinkContract = IGitHubLink(_address);
     }
 
-    function _linkedAccount(string memory gitHubUsername) private view returns (address) {
-        return _gitHubLinkContract.linkedAccount(gitHubUsername);
+    function _linkedAccount(string memory githubUsername) private view returns (address) {
+        return _githubLinkContract.linkedAccount(githubUsername);
     }
-
 
 }
